397157a82d22b3281150ee17f091989e70a1a2cd
Alexandre Besombes <alexandrebesombes@MBPdeAlexandre.home>
2020-11-17 22:21:06 +0100

    libft part 1 done

---
+++ libft/srcs/ft_memchr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abesombe <abesombe@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/17 14:15:21 by abesombe          #+#    #+#             */
+/*   Updated: 2020/11/17 21:42:32 by abesombe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char	*str;
+	size_t			i;
+
+	i = 0;
+	str = (unsigned char*)s;
+	while (i < n)
+	{
+		if (*str == (unsigned char)c)
+			return ((void *)(s + i));
+		str++;
+		i++;
+	}
+	return (NULL);
+}
--- libft/srcs/libft.h
+++ libft/srcs/libft.h
@@ -6,7 +6,7 @@
 /*   By: alexandre <marvin@42.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/15 11:40:20 by alexandre         #+#    #+#             */
-/*   Updated: 2020/11/15 22:23:43 by abesombe         ###   ########.fr       */
+/*   Updated: 2020/11/17 22:20:27 by abesombe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,9 @@
 size_t	ft_strlen(const char *s);
 void	ft_bzero(void *s, size_t n);
 int		ft_isalpha(int c);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 
 #endif
